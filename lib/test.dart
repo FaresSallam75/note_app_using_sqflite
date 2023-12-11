3bda6b50c99b3c4d87e3e10dcac156e74edae549
ACCESS <FaresSallam75>
2023-12-11 03:49:35 +0200

    from scholes

---
+++ lib/test.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
